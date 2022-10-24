297b70991a0270351217d83e56e550a78627a0ad
SAILLARD Quentin <saillardq@gmail.com>
2022-10-24 16:13:16 +0200

    le nouveau changement

--- src/Controller/PictureController.php
+++ src/Controller/PictureController.php
@@ -3,11 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Picture;
+use App\Repository\PictureRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PictureController extends AbstractController
 {
@@ -20,9 +24,14 @@ class PictureController extends AbstractController
         ]);
     }
 
+    #[Route('/api/pictures/{idPicture}', name: 'pictures.get', methods: ['GET'], )]
+    public function getPicture(int $idPicture, PictureRepository $pictureRepository,SerializerInterface $serializer,UrlGeneratorInterface $urlGenerator) : JsonResponse
+    {
+
+    }
 
     #[Route('/api/pictures', name: 'pictures.create', methods: ['POST'], )]
-    public function createPicture(Request $request, entityManagerInterface $entityManager) : JsonResponse
+    public function createPicture(ValidatorInterface $validator, EntityManagerInterface $entityManager,Request $request,SerializerInterface $serializer,UrlGeneratorInterface $urlGenerator) : JsonResponse
     {
         //dd($request);
         $picture = new Picture();
@@ -30,7 +39,12 @@ class PictureController extends AbstractController
         $picture->setFile($files);
         $picture->setMimeType($files->getClientMimeType());
         $picture->setPictureName($files->getClientOriginalName());
+        $picture->setPublicPath("fefefe");
+        $picture->setStatus("on");
         $entityManager->persist($picture);
         $entityManager->flush();
+
+        $location = $urlGenerator->generate("pictures.get", ['idPicture' => $picture->getId()])
+            $jsonPictures = $serializer->serialize($picture)
     }
 }
