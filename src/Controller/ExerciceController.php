d10b13cad56e2b7edb8a4cb836bd2be46bf98e6b
QuentinSAIL <saillardq@gmail.com>
2022-11-18 14:26:29 +0100

    ajout de la fonction2 + ajout de la bdd

--- src/Controller/ExerciceController.php
+++ src/Controller/ExerciceController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use App\Entity\Exercice;
 use App\Repository\ExerciceRepository;
 use App\Repository\MuscleRepository;
+use App\Repository\RegionRepository;
 use JMS\Serializer\SerializationContext;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -51,6 +52,25 @@ class ExerciceController extends AbstractController
         $i = 0;
         while (isset($exerciceMuscle[0][$i])) {
             $programme.= rand(2,4).' repetition de '.$exerciceMuscle[0][$i]["name"].', ';
+            $i++;
+        }
+        $programmeJSON = $serializer->serialize($programme,'json');
+        return new JsonResponse($programmeJSON, Response::HTTP_OK,["accept"=>"json"],true);
+    }
+
+    #[Route('/api/exerciceByRegion/{idRegion}', name: 'exerciceByRegion.get', methods: ['GET'])]
+    public function exerciceByRegion(SerializerInterface $serializer,int $idRegion, MuscleRepository $MuscleRepository, RegionRepository $regionRepository) : JsonResponse
+    {
+        $region = $regionRepository->findOneBy(['id' => $idRegion]);
+        $BDD = new \modele();
+        $exerciceMuscle[] = $BDD->getExerciceByRegion($idRegion);
+        $programme = 'Pour travailler le '.$region->getName().' il faut faire : ';
+        $i = 0;
+        while (isset($exerciceMuscle[0][$i])) {
+            if ($i<>0) {
+                $programme.= ', ';
+            }
+            $programme.= rand(2,4).' repetitions de '.$exerciceMuscle[0][$i]["name"];
             $i++;
         }
         $programmeJSON = $serializer->serialize($programme,'json');
--- src/Services/modele.php
+++ src/Services/modele.php
@@ -6,15 +6,32 @@ class modele
 
     public function __construct()
     {
-        $this->connexion = new PDO('mysql:host=127.0.0.1;dbname=api_muscu_copy_teach',"root","",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $this->connexion = new PDO('mysql:host=127.0.0.1;dbname=api_muscu_copy_teach', "root", "", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
 
-    public function getExerciceByMuscle($IDmuscle) {
+    public function getExerciceByMuscle($IDmuscle)
+    {
         $requetePreparee = $this->connexion->prepare(
             'SELECT * FROM exercice INNER JOIN exercice_muscle ON exercice.id = exercice_muscle.exercice_id 
          INNER JOIN muscle ON muscle.id = exercice_muscle.muscle_id 
-         WHERE muscle_id = :paramIDmuscle');
-        $requetePreparee->bindParam('paramIDmuscle',$IDmuscle);
+         WHERE muscle_id = :paramIDmuscle
+         ORDER BY RAND()
+         LIMIT 5');
+        $requetePreparee->bindParam('paramIDmuscle', $IDmuscle);
+        $requetePreparee->execute();
+        return $requetePreparee->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+    public function getExerciceByRegion($idRegion)
+    {
+        $requetePreparee = $this->connexion->prepare(
+            'SELECT exercice.* FROM exercice INNER JOIN exercice_muscle ON exercice.id = exercice_muscle.exercice_id 
+         INNER JOIN muscle ON muscle.id = exercice_muscle.muscle_id INNER JOIN region ON muscle.region_id_id = region.id
+         WHERE region.id = :paramIdRegion
+         ORDER BY RAND()
+         LIMIT 10');
+        $requetePreparee->bindParam('paramIdRegion', $idRegion);
         $requetePreparee->execute();
         return $requetePreparee->fetchAll(PDO::FETCH_ASSOC);
     }
